2040e83647f26da94a0fd4dc08b49b501b55d812
unknown <SanuMarma.sanumarma1717@gmail.comgit config --global user.email SanuMarma.sanumarma1717@gmail.com>
2025-01-24 12:45:48 +0600

    Allowed Host

--- mamar_bank/settings.py
+++ mamar_bank/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://mamar-bank-wp1z.onrender.com']
 
 
 # Application definition
