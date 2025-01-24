c32ecb3f778fe3590d56be1ad7f40eafb3406f37
unknown <SanuMarma.sanumarma1717@gmail.comgit config --global user.email SanuMarma.sanumarma1717@gmail.com>
2025-01-24 19:40:21 +0600

    password changes

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from . import views
 from .views import UserRegistrationView, UserLoginView, UserLogoutView, UserBankAccountUpdateView
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
-    path('profile/', UserBankAccountUpdateView.as_view(), name='profile' )
+    path('profile/', UserBankAccountUpdateView.as_view(), name='profile'),
+    path('pass_change/', views.pass_change, name='pass_change')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render, redirect
 from django.views.generic import FormView
 from .forms import UserRegistrationForm, UserUpdateForm
 from django.contrib.auth import login, logout
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth.views import LoginView, LogoutView
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
 from django.views import View
 from django.contrib import messages
 from django.urls import reverse_lazy
@@ -10,6 +14,15 @@ from django.urls import reverse_lazy
 from django.views import generic
 
 # Create your views here.
+
+def send_transaction_email(user, subject, template):
+        message = render_to_string(template, {
+            'user' : user,
+        })
+        send_email = EmailMultiAlternatives(subject, '', to=[user.email])
+        send_email.attach_alternative(message, "text/html")
+        send_email.send()
+
 class UserRegistrationView(FormView):
     template_name = 'accounts/user_registration.html'
     form_class = UserRegistrationForm
@@ -37,13 +50,7 @@ class UserLogoutView(generic.View):
     def get(self, request):
         logout(request)
         return redirect('home')
-    
 
-# class UserLogoutView(LogoutView):
-
-#     def get(self, request):
-#         logout(request)
-#         return redirect('login')
     
 class UserBankAccountUpdateView(View):
     template_name = 'accounts/profile.html'
@@ -59,5 +66,18 @@ class UserBankAccountUpdateView(View):
             return redirect('profile')  # Redirect to the user's profile page
         return render(request, self.template_name, {'form': form})
     
-    
+def pass_change(request):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            form = PasswordChangeForm(user=request.user, data = request.POST)
+            if form.is_valid():
+                form.save()
+                update_session_auth_hash(request, form.user)
+                send_transaction_email(request.user, "Password Change Message", "accounts/passchange_email.html")
+                return redirect('profile')
+        else:
+            form = PasswordChangeForm(user=request.user)
+        return render(request, 'accounts/pass_change.html', {'form': form})
+    else:
+        return redirect('login')
 
