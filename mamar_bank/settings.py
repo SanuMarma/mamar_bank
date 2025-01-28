28ef964e907b93f5152b5debdc4dc382101b0ee1
unknown <SanuMarma.sanumarma1717@gmail.comgit config --global user.email SanuMarma.sanumarma1717@gmail.com>
2025-01-29 00:42:22 +0600

    changes done

--- mamar_bank/settings.py
+++ mamar_bank/settings.py
@@ -91,21 +91,21 @@ WSGI_APPLICATION = 'mamar_bank.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Database documentation https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://mamarbank_2btd_user:gSVi7LUK0r6TyHx6oUR6wVMEFzgOzVYt@dpg-cu9jrj9u0jms73ffvks0-a.oregon-postgres.render.com/mamarbank_2btd',        
-    )
-}
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default='postgresql://mamarbank_2btd_user:gSVi7LUK0r6TyHx6oUR6wVMEFzgOzVYt@dpg-cu9jrj9u0jms73ffvks0-a.oregon-postgres.render.com/mamarbank_2btd',        
+#     )
+# }
 
 # DATABASES = {
 #     'default': {
